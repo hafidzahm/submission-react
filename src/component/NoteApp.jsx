c99af4acfccdc3ae3a200c50a51ea122a0825a30
Abdul <hafizhahm123@gmail.com>
2024-09-30 06:18:16 +0700

    feat:addNotes

--- src/component/NoteApp.jsx
+++ src/component/NoteApp.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NoteList from "./NoteList";
 import { getInitialData } from "../initial";
+import NoteForm from "./note-form/NoteForm";
 
 class NoteApp extends React.Component {
     constructor(props) {
@@ -11,6 +12,7 @@ class NoteApp extends React.Component {
         };
 
         this.onDeleteHandler = this.onDeleteHandler.bind(this);
+        this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
     }
 
     onDeleteHandler(id) {
@@ -18,10 +20,30 @@ class NoteApp extends React.Component {
         this.setState({notes})
     }
 
+    onAddNoteHandler({title, body}) {
+        this.setState((prevState) => {
+            return {
+                notes: [
+                    ...prevState.notes,
+                    {
+                        id: +new Date(),
+                        title,
+                        body,
+                        createdAt: new Date().toISOString(),
+                        archived: false,
+                    }
+                ]
+            }
+        });
+    }
+
     render() {
         return (
             <div className="note-app">
-                <h1>Daftar Catatan</h1>
+                <h1>Aplikasi Catatan</h1>
+                <h2>Tambah Catatan</h2>
+                <NoteForm addNote ={this.onAddNoteHandler} />
+                <h2>Daftar Catatan</h2>
                 <NoteList notes={this.state.notes} onDelete={this.onDeleteHandler} />
             </div>
         )
---
+++ src/component/note-form/NoteForm.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+class NoteForm extends React.Component{
+        constructor(props) {
+            super(props);
+
+            this.state = {
+                title: '',
+                body: '',
+            }
+
+            this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
+            this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
+            this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this)
+
+
+        }
+
+        onTitleChangeEventHandler(event) {
+            this.setState(() => {
+                return {
+                    title: event.target.value,
+                }
+            });
+        }
+
+        onBodyChangeEventHandler(event) {
+            this.setState(() => {
+                return {
+                    body: event.target.value,
+                }
+                
+            });
+        }
+
+        onSubmitEventHandler(event) {
+                event.preventDefault();
+                this.props.addNote(this.state);
+        }
+
+        render() {
+            return(
+                <form className="note-form" onSubmit={this.onSubmitEventHandler}>
+                    <input type="text" placeholder="Title" value={this.state.title} onChange={this.onTitleChangeEventHandler}/>
+                    <input type="text" placeholder="Body" value={this.state.body} onChange={this.onBodyChangeEventHandler}/>
+                    <button type="submit">Tambah Catatan</button>
+                </form>
+            )
+        }
+
+        }
+
+
+
+export default NoteForm;
