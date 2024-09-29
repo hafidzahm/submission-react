8a124c3e804043590d4b83647dbfb187c56ba4fb
Abdul <hafizhahm123@gmail.com>
2024-09-29 17:56:13 +0700

    delete-button

--- src/component/NoteApp.jsx
+++ src/component/NoteApp.jsx
@@ -2,15 +2,31 @@ import React from "react";
 import NoteList from "./NoteList";
 import { getInitialData } from "../initial";
 
-function NoteApp() {
-    const notes = getInitialData();
+class NoteApp extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div className="note-app">
-            <h1>Daftar Catatan</h1>
-            <NoteList notes={notes} />
-        </div>
-    )
+        this.state = {
+            notes: getInitialData()
+        };
+
+        this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    }
+
+    onDeleteHandler(id) {
+        const notes = this.state.notes.filter(note => note.id !== id);
+        this.setState({notes})
+    }
+
+    render() {
+        return (
+            <div className="note-app">
+                <h1>Daftar Catatan</h1>
+                <NoteList notes={this.state.notes} onDelete={this.onDeleteHandler} />
+            </div>
+        )
+
+    }
 }
 
 export default NoteApp;
--- src/component/NoteItem.jsx
+++ src/component/NoteItem.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NoteItemBody from "./NoteItemBody";
+import DeleteButton from "./button/DeleteButton";
 
-function NoteItem ({ title, body, createdAt, archived}) {
+function NoteItem ({ id, title, body, createdAt, archived, onDelete }) {
     return (
         <div className="note-item">
             <NoteItemBody title={title} body={body} createdAt={createdAt} archived={archived}/>
+            <DeleteButton id={id} onDelete={onDelete} />
         </div>
     );
 }
 
-export default NoteItem;
+export default NoteItem;    
